4f809bd7e171f3b48dc27a0b955c92d28aa4f85d
Mathieu <dev1@MacBook-Pro-dev.local>
2021-09-10 11:05:18 +0200

    Centralisation de la configuration

--- assets/services/customersAPI.js
+++ assets/services/customersAPI.js
@@ -1,34 +1,64 @@
 import axios from "axios";
+import Cache from "./cache";
+import { CUSTOMERS_API } from "../styles/config";
 
-function findAll() {
+async function findAll() {
+
+    const cachedCustomers = await Cache.get("customers");
+
+    if (cachedCustomers) return cachedCustomers;
+
     return axios
-        .get("http://127.0.0.1:8000/api/customers")
-        .then(response => response.data['hydra:member']);
+        .get(CUSTOMERS_API)
+        .then(response => {
+            const customers = response.data['hydra:member'];
+            Cache.set("customers", customers);
+            return customers;
+        });
 }
 
 function find(id) {
     return axios
-        .get("http://localhost:8000/api/customers/" + id)
-        .then(response => response.data);
+        .get(CUSTOMERS_API + "/" + id)
+        .then(async response => {
+            const cachedCustomers = await Cache.get("customers");
+
+            if (cachedCustomers) {
+                Cache.set("customers", cachedCustomers.filter(c => c.id !== id));
+            }
+
+            return response;
+        });
 }
 
 function deleteCustomer(id) {
     return axios
-        .delete("http://127.0.0.1:8000/api/customers/" + id);
+        .delete(CUSTOMERS_API + "/" + id);
 }
 
 function update(id, customer) {
     return axios.put(
-        "http://localhost:8000/api/customers/" + id,
+        CUSTOMERS_API + "/" + id,
         customer
-    );
+    ).then(response => {
+        Cache.invalidate("customers");
+        return response;
+    });
 }
 
 function create(customer) {
     return axios.post(
-        "http://localhost:8000/api/customers",
+        CUSTOMERS_API,
         customer
-    );
+    ).then(async response => {
+        const cachedCustomers = await Cache.get("customers");
+
+        if (cachedCustomers) {
+            Cache.set("customers", [...cachedCustomers, response.data]);
+        }
+
+        return response;
+    });
 }
 
 export default {
